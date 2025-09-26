b0ca34e4e0c13f628370a1f4a0b226e4e5c64d98
Amazonka <polinavinnikova0@gmail.com>
2025-09-26 11:08:49 +0300

    Change header color

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -24,7 +24,7 @@ export default function Header() {
   };
 
   return (
-    <AppBar position="relative">
+    <AppBar position="relative" sx={{ backgroundColor: "#b19779ff" }}>
       <Toolbar>
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           <Link
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Store!
+            Amazonka's Store
           </Link>
         </Typography>
 
